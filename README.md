efb07b25abc050c2f3b5c7f1e70c0ae2689f6094
zayazzp <pzayazz@gmail.com>
2021-06-20 15:55:32 +0545

    Mongoose Connection

---
+++ README.md
@@ -0,0 +1,5 @@
+# Everest Book API
+Create env file
+```python
+DB_URI=<URI_STRING>
+```
---
+++ utils/dbConnection.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose")
+require('dotenv').config
+mongoose.connect(process.env.DB_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+})
