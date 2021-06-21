50841a1bbf7fa27227603e6561022c9d342d4282
zayazzp <pzayazz@gmail.com>
2021-06-21 15:06:18 +0545

    Modified README.md

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Everest Book API
-Create env file
+Create env file with
 ```python
+# Use your atlas URI
 DB_URI=<URI_STRING>
 ```
+## Run without env
+change value of URI in **utils/dbCollection.js**
--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose")
 require('dotenv').config()
-mongoose.connect(process.env.DB_URI,{
+// if .env is not defined, process.env.DB_URI -> undefined
+// Change URI to "127.0.0.1:27017" if you are using Mongodb Community Server
+const URI = process.env.DB_URI 
+mongoose.connect(URI,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
