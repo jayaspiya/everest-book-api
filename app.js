d3f1831a2dbaaa6200867feca6414dbfd91bb3a2
zayazzp <pzayazz@gmail.com>
2021-07-16 11:37:08 +0545

    Express static uploads

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger)
 // app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-
+app.use(express.static("./uploads"))
 
 app.use("/user",userRoutes)
 app.use('/store',storeRoutes)
