4947f81ff443b1577106d04cd68395190701e704
zayazzp <pzayazz@gmail.com>
2021-09-02 13:21:17 +0545

    :goal_net: bookController Method Error Handling

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,53 +1,131 @@
 const Book = require("../models/Book.js")
 const cloudinary = require("../utils/cloudinary.js")
 const slug = require("../utils/slug.js")
+const {success, failure} = require("../utils/messageJson.js")
 
-exports.get_all_books = async function(_,res){
-    const bookList = await Book.find()
-    res.json({
-        message: "Get all book",
-        success: true,
-        data: bookList
-    })
+exports.get_all_books = async function(req,res){
+    try {
+        // Default: Decending Order [Newest to Oldest] 
+        let sort = -1
+        // Check if request query oldest has truthy value
+        if(!!req.query.oldest){
+            // Ascending Order [Oldest to Newest]
+            sort = 1
+        }
+        const bookList = await Book.find().sort({addedDate: sort})
+        res.json(success("Get All Books", bookList))
+    } catch (error) {
+        console.log(error)
+        res.json(failure())
+    }
     res.end()
 }
 
 exports.get_book = async function(req, res){
     try {
-        const book = await Book.findById(req.params.bookId)
+        const _id = req.params.bookId
+        const book = await Book.findById(_id)
         if(book){
-            res.json({
-                message: "Book found",
-                success: true,
-                data: [book]
-            })
+            res.json(success("Book Found", [book]))
         }
         else{
-            res.json({
-                message: "Book not found",
-                success: false,
-            })
+            res.json(failure("Book Not Found"))
         }
     } catch (error) {
-        res.json({
-            message: "Id not valid",
-            success: false
-        })    
+        console.log(error)
+        res.json(failure())  
     }
     res.end()
 }
 
-exports.get_books_without_cover = async function(req,res){
-    const defaultCover = "https://res.cloudinary.com/zayazzp/image/upload/c_crop,g_east,h_400,w_250/v1629858102/no-cover_eh5nrb.png"
-    const bookList = await Book.find({"cover.front": defaultCover})
-    res.json({
-        message: "Successful",
-        success: true,
-        data: bookList
-    })
+exports.get_books_without_cover = async function(_,res){
+    try {
+        // Default Value of Book.cover.front
+        const defaultCover = "https://res.cloudinary.com/zayazzp/image/upload/c_crop,g_east,h_400,w_250/v1629858102/no-cover_eh5nrb.png"
+        const bookList = await Book.find({"cover.front": defaultCover})
+        res.json(success("Successful", bookList))
+    } catch (error) {
+        console.log(error)
+        res.json(failure())
+    }
     res.end()
 }
 
+exports.insert_new_book = async function(req,res){
+    try {
+        const book = new Book({
+            title: req.body.title,
+            author: req.body.author,
+            isbn: req.body.isbn,
+            synopsis: req.body.synopsis,
+            tags: req.body.tags,
+            price: req.body.price,
+            releasedYear: req.body.releasedYear
+        })
+        await book.save()
+        res.json(success(`${req.body.title} Book Added.`))
+    } catch (error) {
+        console.log(error)
+        res.json(failure())
+    }
+    res.end()
+}
+
+exports.update_book_detail = async function(req, res){
+    try {
+        await Book.updateOne({
+            _id: req.body._id
+        },{
+            tags: req.body.tags,
+            title: req.body.title,
+            author: req.body.author,
+            isbn: req.body.isbn,
+            synopsis: req.body.synopsis,
+            price: req.body.price,
+            releasedYear: req.body.releasedYear
+        })
+        res.json(success("Book Updated"))
+    } 
+    catch (error) {
+        console.log(error)
+        res.json(failure())
+    }
+    res.end()
+}
+
+exports.delete_book = async function(req,res){
+    try{
+        await Book.deleteOne({
+            _id: req.params.id
+        })
+        res.json(success())
+    }
+    catch(error){
+        console.log(error)
+        res.json(failure())
+    }
+    res.end()
+}
+
+exports.update_cover_image = async function(req,res){
+    try {
+        // TODO: Filter Book by Mimetype
+        const formImage = req.files.cover
+        console.log(formImage)
+        const imagePath = formImage.tempFilePath
+        const bookName = req.params.bookId
+        const cover = await cloudinary.uploadBookCover(imagePath, bookName)
+        const _id = req.params.bookId
+        await Book.updateOne({_id},{cover})
+        res.json(success("Book Cover Changed"))
+    } catch (error) {
+        conosle.log(error)
+        res.json(failure())
+    }
+    res.end()
+}
+// Searching [Unused fo now] 
+// TODO: Need To Change Searching
 exports.get_book_by_title = async function(req, res){
     const bookTitle = slug(req.params.bookTitle).toLowerCase()
     const bookList = await Book.findOne({title: bookTitle})
@@ -87,85 +165,6 @@ exports.get_book_by_tags = async function (req,res){
         message: "Successful",
         success: true,
         data: bookList
-    })
-    res.end()
-}
-
-exports.get_latest_books = async function(req,res){
-    const bookList = await Book.find().sort({addedDate: -1})
-    res.json({
-        message: "Successful",
-        success: true,
-        data: bookList
-    })
-    res.end()
-}
-
-exports.insert_new_book = async function(req,res){
-    const book = new Book({
-        title: req.body.title,
-        author: req.body.author,
-        isbn: req.body.isbn,
-        synopsis: req.body.synopsis,
-        tags: req.body.tags,
-        price: req.body.price,
-        releasedYear: req.body.releasedYear
-    })
-    await book.save()
-    res.json({
-        message: `${req.body.title} Book Added.`,
-        success: true,
-    })
-    res.end()
-}
-
-exports.update_book_detail = async function(req, res){
-    await Book.updateOne({
-        _id: req.body._id
-    },{
-        tags: req.body.tags,
-        title: req.body.title,
-        author: req.body.author,
-        isbn: req.body.isbn,
-        synopsis: req.body.synopsis,
-        price: req.body.price,
-        releasedYear: req.body.releasedYear
-    })
-    res.json({
-        message: "Book Updated.",
-        success: true,
-    })
-    res.end()
-}
-
-exports.delete_book_by_id = async function(req,res){
-    await Book.deleteOne({
-        _id: req.params.id
-    })
-    res.json({
-        message: "Book Deleted.",
-        success: true,
-    })
-    res.end()
-}
-
-exports.update_cover_image = async function(req,res){
-    // Cover filename
-    // TODO: Filter Book by Mimetype
-    const formImage = req.files.cover
-    console.log(formImage)
-    const imagePath = formImage.tempFilePath
-    const bookName = req.params.bookId
-    const result = await cloudinary.uploadBookCover(imagePath, bookName)
-    const bookId = req.params.bookId
-    await Book.updateOne({
-        _id: bookId
-    },{
-        cover: result
-    })
-    res.json({
-        message: "Book Cover Updated.",
-        success: true,
     })
     res.end()
 }
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,7 +6,20 @@ router.get("/", bookController.get_all_books)
 
 router.get('/view/:bookId', bookController.get_book)
 
-router.get("/latest", bookController.get_latest_books)
+// [For Admin Interface] 
+
+router.get("/books-without-cover", bookController.get_books_without_cover)
+
+// [User Verification Required] 
+router.post("/", auth.verifyStore, bookController.insert_new_book)
+
+router.patch("/cover/:bookId", auth.verifyStore, bookController.update_cover_image)
+
+router.put("/", auth.verifyStore, bookController.update_book_detail)
+
+router.delete("/:id", auth.verifyStore, bookController.delete_book)
+
+// Searching  Unused for now
 
 router.get("/author/:authorName", bookController.get_book_by_author)
 
@@ -15,16 +28,5 @@ router.get("/isbn/:isbn", bookController.get_book_by_isbn)
 router.get('/tags', bookController.get_book_by_tags)
 
 router.get('/title/:bookId', bookController.get_book_by_title)
-
-router.get("/books-without-cover", bookController.get_books_without_cover)
-
-// [User Verification Required] 
-router.post("/", auth.verifyStore, bookController.insert_new_book)
-
-router.put("/cover/:bookId", auth.verifyStore, bookController.update_cover_image)
-
-router.put("/", auth.verifyStore, bookController.update_book_detail)
-
-router.delete("/:id", auth.verifyStore, bookController.delete_book_by_id)
 
 module.exports = router
